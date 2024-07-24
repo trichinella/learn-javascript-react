3ca3fdc0ebc37cc80f86350c798256dc1b7ff57d
trihin <a.trichinella@gmail.com>
2024-07-24 19:35:03 +0300

    правки по код-ревью

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,13 @@
-import Button from "../button/Button.jsx";
-import { useThemeContext } from "../themeProvider/ThemeProvider.jsx";
 import Auth from "../auth/Auth.jsx";
 import styles from "./styles.module.css";
+import ToggleButton from "../toggleButton/ToggleButton.jsx";
 
 const Header = () => {
-    const {toggle} = useThemeContext();
-
     return (
         <header>
             <div className={styles.headerPanel}>
                 <div className={styles.mainPanel}>
-                    <Button onClick={() => {
-                        toggle();
-                    }}>
-                        Toggle theme
-                    </Button>
+                    <ToggleButton/>
                 </div>
                 <Auth/>
             </div>
