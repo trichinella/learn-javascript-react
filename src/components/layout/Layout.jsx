9b4d61bf8e91f49a47903c63892fb322a3041b3a
trihin <a.trichinella@gmail.com>
2024-08-10 11:19:28 +0300

    lecture 11 - homework 10

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,13 +4,14 @@ import Footer from "../footer/Footer.jsx";
 import { Outlet } from "react-router-dom";
 import ScrollProgressBar from "../scrollProgressBar/ScrollProgressBar.jsx";
 import UserFlowSection from "../userFlowSection/UserFlowSection.jsx";
+import styles from "./styles.module.css";
 
 export const Layout = () => {
     return (
-        <div className={"container"}>
+        <div className={styles.container}>
             <Header/>
             <ScrollProgressBar/>
-            <main>
+            <main className={styles.main}>
                 <Outlet/>
             </main>
             <UserFlowSection/>
